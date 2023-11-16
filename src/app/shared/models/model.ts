c11089be97a9b41ffb5ccbe488d812ae6709f0ee
Roger Martin <rdogmartin@gmail.com>
2023-11-16 10:50:01 -0700

    Add rudimentary auth

---
+++ src/app/shared/models/model.ts
@@ -0,0 +1,11 @@
+export interface CurrentUser {
+  userId: string;
+  userRoles: string[];
+  identityProvider: string;
+  userDetails: string;
+  claims: string[];
+}
+
+export interface AuthResult {
+  clientPrincipal: CurrentUser;
+}
