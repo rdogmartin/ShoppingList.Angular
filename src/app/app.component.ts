c49bfcee32c8908f1da48e9379dfe442f2b9e17a
Roger Martin <rdogmartin@gmail.com>
2023-11-15 17:18:02 -0700

    Add AppInfo endpoint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,38 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient, HttpClientModule, HttpHeaders } from '@angular/common/http';
+import { catchError, of, tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  message = '';
   title = 'ShoppingList.Angular';
+
+  constructor(private http: HttpClient) {
+    var httpOptions = {
+      headers: new HttpHeaders({
+        'Accept': 'text/html, application/xhtml+xml, */*',
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }),
+      responseType: 'text' as 'json'
+    };
+
+    this.http.get<string>('http://localhost:7071/api/AppInfo?name=Roger', httpOptions).pipe(
+      tap((resp) => {
+        console.log(resp);
+        this.message = resp;
+      }),
+      catchError((err) => {
+        console.error(err.error.error.message);
+        return of('Error');
+      }),
+    ).subscribe();
+  }
 }
