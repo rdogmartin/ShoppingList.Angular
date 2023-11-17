ca393deb77858a37fe5d57d4b033f9e06af8dcab
Roger Martin <rdogmartin@gmail.com>
2023-11-17 15:46:34 -0700

    Add ESLint and prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,20 +11,31 @@ import { toSignal } from '@angular/core/rxjs-interop';
   standalone: true,
   imports: [CommonModule, RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  currentUser = toSignal(this.getUser(), { initialValue: { userId: '', userRoles: [], identityProvider: '', userDetails: '', claims: [] }, requireSync: false});
+  currentUser = toSignal(this.getUser(), {
+    initialValue: { userId: '', userRoles: [], identityProvider: '', userDetails: '', claims: [] },
+    requireSync: false,
+  });
   appInfo = toSignal(this.getAppInfo());
   message = '';
   title = 'ShoppingList.Angular';
 
-  constructor(private http: HttpClient) {
-  }
+  constructor(private http: HttpClient) {}
 
   private getUser() {
     return this.http.get<AuthResult>('/.auth/me').pipe(
-      map((authResult) => authResult.clientPrincipal || { userId: '', userRoles: [], identityProvider: '', userDetails: '', claims: [] }),
+      map(
+        (authResult) =>
+          authResult.clientPrincipal || {
+            userId: '',
+            userRoles: [],
+            identityProvider: '',
+            userDetails: '',
+            claims: [],
+          },
+      ),
     );
   }
 
