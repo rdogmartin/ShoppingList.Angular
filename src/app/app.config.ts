e59edad5acee5693e3297ec3fdbd34e4a1a1c43c
Roger Martin <rdogmartin@gmail.com>
2023-11-20 10:11:35 -0700

    Revert "Add Angular Material"
    This reverts commit 80f8da236c700876f5dc3d912f851867fa0a53fe.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,8 +4,7 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
-import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(), provideAnimations()],
+  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient()],
 };
