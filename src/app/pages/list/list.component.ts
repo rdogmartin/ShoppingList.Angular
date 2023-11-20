0656627f1e4c77dc2d56b5dae00985bb5be67812
Roger Martin <rdogmartin@gmail.com>
2023-11-20 08:28:39 -0700

    Add simple checkbox

--- src/app/pages/list/list.component.ts
+++ src/app/pages/list/list.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../shared/services/auth.service';
 import { toSignal } from '@angular/core/rxjs-interop';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCheckboxModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss',
 })
