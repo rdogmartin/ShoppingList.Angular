6e5fbd94af2aa3b590caaa0603784bde43c1c9a9
Roger Martin <rdogmartin@gmail.com>
2023-11-27 17:19:07 -0700

    Retrieve list from Cosmos DB

---
+++ api/Dto/UserListItems.cs
@@ -0,0 +1,8 @@
+﻿namespace Api.Dto;
+
+/// <summary>
+/// Represents an item in a list.
+/// </summary>
+/// <param name="Id">A value uniquely identifying the item. Ex: rdmartin33@hotmail.com</param>
+/// <param name="ListItems">The list of items belonging to the user.</param>
+public record class UserListItems(string Id, ListItem[] ListItems) { }
