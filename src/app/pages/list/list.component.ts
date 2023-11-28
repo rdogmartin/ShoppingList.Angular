0d33d692fece903ba06cc14fc6c0dba26621b265
Roger Martin <rdogmartin@gmail.com>
2023-11-28 10:51:02 -0700

    Add spinner while HTTP call to get items is executing

--- src/app/pages/list/list.component.ts
+++ src/app/pages/list/list.component.ts
@@ -2,13 +2,14 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { AuthService } from '../../shared/services/auth.service';
 import { ListItemService } from '../../shared/services/listItem.service';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CommonModule, MatCheckboxModule],
+  imports: [CommonModule, MatCheckboxModule, MatProgressSpinnerModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss',
 })
@@ -19,7 +20,7 @@ export class ListComponent {
   });
 
   userListItems = toSignal(this.listItemService.getListItems(), {
-    initialValue: [],
+    initialValue: null,
     requireSync: false,
   });
 
