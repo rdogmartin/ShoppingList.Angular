d0bedee879bff0a24b544cba562753d6e1c530a2
Roger Martin <rdogmartin@gmail.com>
2025-03-23 19:00:56 -0600

    Add snowman menu with logout

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,10 +1,32 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { AuthService } from '../../services/auth.service';
+import { BrowserLocalStorageService } from '../../services/browser-local-storage.service';
+import { BrowserService } from '../../services/browser.service';
 
 @Component({
   selector: 'app-header',
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule, MatMenuModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  public isLoggedIn = toSignal(this.authService.isLoggedIn$, {
+    initialValue: false,
+    rejectErrors: true,
+  });
+
+  constructor(
+    private authService: AuthService,
+    private browserLocalStorageService: BrowserLocalStorageService,
+    private browserService: BrowserService,
+  ) {}
+
+  public logout() {
+    this.browserLocalStorageService.clear();
+    this.browserService.redirectToPage('/');
+  }
+}
